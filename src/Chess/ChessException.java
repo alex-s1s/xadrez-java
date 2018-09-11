1007cc6705b4fe34b42bcd8edfc82f3a238cae8c
Alex silva <alexdean@outlook.com.br>
2018-09-11 09:42:55 -0300

    Movinf movies

--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -1,12 +1,13 @@
 package Chess;
 
-public class ChessException extends RuntimeException {
+import boardGame.BoardException;
+
+public class ChessException extends BoardException {
 
     private static final long seriaLVersionUID = 1L;
 
     public ChessException(String msg) {
         super(msg);
     }
-    
-    
+
 }
