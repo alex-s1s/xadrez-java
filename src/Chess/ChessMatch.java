f1de1d6ec182f771bb85a0f98c2d8628cb5f306d
Alex silva <alexdean@outlook.com.br>
2018-09-17 10:20:48 -0300

    Handling captured pieces

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -5,12 +5,17 @@ import Chess.pieces.Rook;
 import boardGame.Board;
 import boardGame.Piece;
 import boardGame.Position;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -57,11 +62,15 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+        
         return capturedPiece;
     }
-    
-    
-    
+
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("Não existe peça na posição de origem.");
@@ -89,8 +98,11 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
-
+    
+    
+    
     private void initialSetup() {
 
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,9 @@ import Chess.ChessException;
 import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -12,29 +14,33 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
+
         while (true) {
             try {
                 UI.clearscreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.println("Origem: ");
                 ChessPosition source = UI.readChessPosition(sc);
-                
+
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearscreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-                
+
                 System.out.println();
                 System.out.println("Destino: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.perfomeChessMove(source, target);
-            }
-            catch(ChessException e){
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+
+            } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
